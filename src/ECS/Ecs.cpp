4b5f44923d9e678bd22c58f276641dbe61003747
Peter Crabbe <petercrabbe92@gmail.com>
2020-07-23 22:32:28 +0800

    encapsulate ECS

--- src/ECS/Ecs.cpp
+++ src/ECS/Ecs.cpp
@@ -1,5 +1,6 @@
 #include "Ecs.hpp"
 #include "ECS/System/Draw.hpp"
+#include "ECS/Entity.hpp"
 
 void ECS::FixedUpdate(double t, double dt) {
 
@@ -12,3 +13,9 @@ void ECS::Update(double t, double dt) {
 void ECS::Draw(const glm::mat4& projection, const glm::mat4& view, const glm::dvec2& cameraPos) {
     System::Draw(registry, projection, view);
 }
+
+Entity &ECS::CreateEntity() {
+    this->entityRegister.emplace_back(std::make_shared<Entity>(registry.create(), this));
+    return *entityRegister.at(entityRegister.size() - 1).get();
+}
+
--- src/Game/Demo/Demo.cpp
+++ src/Game/Demo/Demo.cpp
@@ -1,3 +1,4 @@
+#include "ECS/Entity.hpp"
 #include "Demo.hpp"
 #include "Engine/Engine.hpp"
 #include "ECS/Component/Model.hpp"
@@ -7,10 +8,10 @@ Demo::Demo() {
     camera = Engine::Camera();
     camera.Position = glm::vec3(0.0f, 10.0f, 0.0f);
     relativeMouse = true;
-    auto entity = ecs.registry.create();
+    auto entity = ecs.CreateEntity();
     auto shader = std::make_shared<Shader>(Shader("res/shader/vertshader.vs", "res/shader/fragshader.fs"));
-    ecs.registry.emplace<Component::Model>(entity, "res/model/ClothedMan.gltf", shader);
-    auto &tran = ecs.registry.emplace<Component::Transform>(entity);
+    entity.AddComponent<Component::Model>("res/model/ClothedMan.gltf", shader);
+    auto &tran = entity.AddComponent<Component::Transform>();
     tran.pos.x = 20.0f;
     tran.pos.z = 2.0f;
 }

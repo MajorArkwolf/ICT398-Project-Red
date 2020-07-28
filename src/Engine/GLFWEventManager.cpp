a03502a3cb10521ec1dab958c36c85341c08fbd1
KieraG <KieraJGibson0@gmail.com>
2020-07-28 14:11:27 +0800

    Start of event manager

--- src/Engine/GLFWEventManager.cpp
+++ src/Engine/GLFWEventManager.cpp
@@ -1,1 +1,94 @@
 #include "GLFWEventManager.h"
+
+void GLFWEventManager::gleqInit(void)
+{
+    glfwSetMonitorCallback(gleq_monitor_callback);
+    glfwSetJoystickCallback(gleq_joystick_callback);
+
+}
+
+void Input::GLFWEventManager::Init(GLFWwindow* window)
+{
+}
+
+void Input::GLFWEventManager::TrackWindow(GLFWwindow* window)
+{
+}
+
+void Input::GLFWEventManager::WindowPosCallback(GLFWwindow* window, int x, int y)
+{
+    GLFWEvent event;
+    event.Type = GLFWEventType::WindowMoved;
+    event.Window = window;
+    event.Position.X = x;
+    event.Position.Type = y;
+    EventQueue.Emplace(event);
+}
+
+void Input::GLFWEventManager::WindowSizeCallback(GLFWwindow* window, int width, int height)
+{
+}
+
+void Input::GLFWEventManager::WindowCloseCallback(GLFWwindow* window)
+{
+}
+
+void Input::GLFWEventManager::WindowRefreshCallback(GLFWwindow* window)
+{
+}
+
+void Input::GLFWEventManager::WindowFocusCallback(GLFWwindow* window, int focused)
+{
+}
+
+void Input::GLFWEventManager::WindowIconifyCallback(GLFWwindow* window, int iconified)
+{
+}
+
+void Input::GLFWEventManager::FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+}
+
+void Input::GLFWEventManager::MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+}
+
+void Input::GLFWEventManager::CursorPositionCallback(GLFWwindow* window, double x, double y)
+{
+}
+
+void Input::GLFWEventManager::CursorEnterCallback(GLFWwindow* window, int entered)
+{
+}
+
+void Input::GLFWEventManager::MouseScrollCallback(GLFWwindow* window, double x, double y)
+{
+}
+
+void Input::GLFWEventManager::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+}
+
+void Input::GLFWEventManager::CharCallback(GLFWwindow* window, unsigned codepoint)
+{
+}
+
+void Input::GLFWEventManager::MonitorCallback(GLFWwindow* window, int action)
+{
+}
+
+void Input::GLFWEventManager::FileDropCallback(GLFWwindow* window, int count, const char** paths)
+{
+}
+
+void Input::GLFWEventManager::JoystickCallback(int jid, int action)
+{
+}
+
+void Input::GLFWEventManager::WindowMaximiseCallback(GLFWwindow* window, int maximised)
+{
+}
+
+void Input::GLFWEventManager::WindowContentScaleCallback(GLFWwindow* window, float xscale, float yscale)
+{
+}
--- src/Engine/GLFWEventManager.h
+++ src/Engine/GLFWEventManager.h
@@ -92,9 +92,30 @@ namespace Input
 		~GLFWEventManager() = delete;
 
 		void Init(GLFWwindow* window);
-		
+		void TrackWindow(GLFWwindow* window);
+
 
 	private:
+		static void WindowPosCallback(GLFWwindow* window, int x, int y);
+		static void WindowSizeCallback(GLFWwindow* window, int width, int height);
+		static void WindowCloseCallback(GLFWwindow* window);
+		static void WindowRefreshCallback(GLFWwindow* window);
+		static void WindowFocusCallback(GLFWwindow* window, int focused);
+		static void WindowIconifyCallback(GLFWwindow* window, int iconified);
+		static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+		static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		static void CursorPositionCallback(GLFWwindow* window, double x, double y);
+		static void CursorEnterCallback(GLFWwindow* window, int entered);
+		static void MouseScrollCallback(GLFWwindow* window, double x, double y);
+		static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		static void CharCallback(GLFWwindow* window, unsigned codepoint);
+		static void MonitorCallback(GLFWwindow* window, int action);
+		static void FileDropCallback(GLFWwindow* window, int count, const char** paths);
+		static void JoystickCallback(int jid, int action);
+		static void WindowMaximiseCallback(GLFWwindow* window, int maximised);
+		static void WindowContentScaleCallback(GLFWwindow* window, float xscale, float yscale);
+
+
 		std::queue<Input::InputEvent> EventQueue;
 	};
 }

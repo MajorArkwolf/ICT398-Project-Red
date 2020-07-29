3090e1ca24c6277bc3c045e3110bb2b30feb1dff
Peter <petercrabbe92@gmail.com>
2020-07-29 22:31:45 +0800

    Now it builds on windows

--- src/ECS/Component/Model.hpp
+++ src/ECS/Component/Model.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <string>
+#include <filesystem>
 #include "Engine/Renderer/Shader.hpp"
 
 namespace Component {
     struct Model {
-        Model(const std::string &model, std::shared_ptr<Shader> newShader);
+        Model(const std::filesystem::path &model, std::shared_ptr<Shader> newShader);
         size_t id = 0;
         std::shared_ptr<Shader> shader = nullptr;
     };

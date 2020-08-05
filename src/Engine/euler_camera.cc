abf97de3b0c377b7942f3cd804ca0dbbd85353fd
William Vlahos <wxavierv@gmail.com>
2020-08-05 16:42:11 +0800

    Preliminary style guide compliance changes for euler_camera.h and euler_camera.cc.

---
+++ src/Engine/euler_camera.cc
@@ -0,0 +1,116 @@
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "euler_camera.h"
+
+namespace engine {
+
+// Constructor with vectors
+Camera::Camera(glm::dvec3 position, glm::dvec3 up, double yaw, double pitch)
+    : front_(glm::dvec3(0.0f, 0.0f, -1.0f)), movement_speed_(kSpeed),
+      mouse_sensitivity_(kSensitivity), zoom_(kZoom) {
+  position_ = position;
+  world_up_ = up;
+  yaw_ = yaw;
+  pitch_ = pitch;
+  UpdateCameraVectors();
+}
+
+// Constructor with scalar values
+Camera::Camera(double pos_x, double pos_y, double pos_z, double up_x,
+               double up_y, double up_z, double yaw, double pitch)
+    : front_(glm::dvec3(0.0f, 0.0f, -1.0f)), movement_speed_(kSpeed),
+      mouse_sensitivity_(kSensitivity), zoom_(kZoom) {
+  position_ = glm::dvec3(pos_x, pos_y, pos_z);
+  world_up_ = glm::dvec3(up_x, up_y, up_z);
+  yaw_ = yaw;
+  pitch_ = pitch;
+  UpdateCameraVectors();
+}
+
+glm::mat4 Camera::GetViewMatrix() {
+  return glm::lookAt(position_, position_ + front_, up_);
+}
+
+void Camera::ProcessKeyboard(CameraMovement direction, double delta_time) {
+  const auto velocity =
+      static_cast<float>(movement_speed_ * delta_time * 1000.0);
+  if (direction == CameraMovement::kForward)
+    position_ += front_ * velocity;
+  if (direction == CameraMovement::kBackward)
+    position_ -= front_ * velocity;
+  if (direction == CameraMovement::kLeft)
+    position_ -= right_ * velocity;
+  if (direction == CameraMovement::kRight)
+    position_ += right_ * velocity;
+}
+
+void Camera::ProcessMouseMovement(double x_offset, double y_offset,
+                                  GLboolean constrain_pitch) {
+  x_offset *= mouse_sensitivity_;
+  y_offset *= mouse_sensitivity_;
+
+  yaw_ += x_offset;
+  pitch_ += y_offset;
+
+  // Make sure that when pitch is out of bounds, screen doesn't get flipped
+  if (constrain_pitch) {
+    if (pitch_ > 89.0)
+      pitch_ = 89.0;
+    if (pitch_ < -89.0)
+      pitch_ = -89.0;
+  }
+
+  // Update front_, right_ and up_ Vectors using the updated Euler angles
+  UpdateCameraVectors();
+}
+
+void Camera::ProcessMouseScroll(double y_offset) {
+  if (zoom_ >= 1.0 && zoom_ <= 45.0)
+    zoom_ -= y_offset;
+  if (zoom_ <= 1.0)
+    zoom_ = 1.0;
+  if (zoom_ >= 45.0)
+    zoom_ = 45.0;
+}
+
+void Camera::UpdateCameraVectors() {
+  // Calculate the new front_ vector
+  glm::dvec3 front;
+  front.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+  front.y = sin(glm::radians(pitch_));
+  front.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+  front_ = glm::normalize(front);
+  // Also re-calculate the right_ and up_ vector
+  right_ =
+      glm::normalize(glm::cross(front_, world_up_));
+  // Normalize the vectors, because their length gets
+  // closer to 0 the more you look up or down which results in slower movement.
+  up_ = glm::normalize(glm::cross(right_, front_));
+}
+
+glm::dvec2 Camera::GetLocation() const {
+  auto key = glm::dvec2(position_.x, position_.z);
+  return key;
+}
+
+glm::vec3 Camera::GetRightVector() {
+  // Normalize the vectors, because their length gets
+  // closer to 0 the more you look up or down which results in slower movement.
+  return glm::normalize(glm::cross(front_, world_up_));
+}
+
+void Camera::ProcessKeyboardInput(bool forward, bool backward, bool left,
+                                  bool right, double delta_time) {
+  const auto velocity =
+      static_cast<float>(movement_speed_ * delta_time * 1000.0);
+  if (forward)
+    position_ += front_ * velocity;
+  if (backward)
+    position_ -= front_ * velocity;
+  if (left)
+    position_ -= right_ * velocity;
+  if (right)
+    position_ += right_ * velocity;
+}
+
+}  // namespace engine
---
+++ src/Engine/euler_camera.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <vector>
+
+#include <glad/glad.h>  // NOLINT
+#include <glm/glm.hpp>
+
+/// Makes understanding the direction simple.
+enum class CameraMovement {
+  kForward,
+  kBackward,
+  kLeft,
+  kRight
+};
+
+/// Default values used to initialise a camera.
+constexpr double kYaw = 0.0;
+constexpr double kPitch = 0.0;
+constexpr double kSpeed = 0.05;
+constexpr double kSensitivity = 0.1;
+constexpr double kZoom = 45.0;
+
+namespace engine {
+
+/**
+ * Euler Camera designed to interact in a 3D environment.
+ */
+class Camera {
+ public:
+  /// Camera Attributes
+  glm::vec3 position_ = {};
+  glm::vec3 front_ = {};
+  glm::vec3 up_ = {};
+  glm::vec3 right_ = {};
+  glm::vec3 world_up_ = {};
+
+  /// Euler Angles
+  double yaw_ = {};
+  double pitch_ = {};
+
+  /// Camera options
+  double movement_speed_ = {};
+  double mouse_sensitivity_ = {};
+  double zoom_ = {};
+
+  /**
+   * Default Constructor
+   * @param position the position of the camera.
+   * @param up The up axis
+   * @param yaw of the camera.
+   * @param pitch of the camera.
+   */
+  explicit Camera(glm::dvec3 position = glm::dvec3(0.0f, 0.0f, 0.0f),
+                  glm::dvec3 up = glm::dvec3(0.0f, 1.0f, 0.0f),
+                  double yaw = kYaw,
+                  double pitch = kPitch);
+
+  /**
+   * Overloaded constructor
+   * @param pos_x the position on the x axis.
+   * @param pos_y the position on the y axis.
+   * @param pos_z the position on the z axis.
+   * @param up_x position on the x axis.
+   * @param up_y position on the y axis.
+   * @param up_z position on the z axis.
+   * @param yaw of the camera.
+   * @param pitch of the camera.
+   */
+  Camera(double pos_x, double pos_y, double pos_z, double up_x, double up_y,
+         double up_z, double yaw, double pitch);
+
+  /**
+   * Calculates the view matrix used for shaders and the rendering engine.
+   * @return Gets the view matrix
+   */
+  glm::mat4 GetViewMatrix();
+
+  /**
+   * Applies the movement based on the default values and delta time.
+   * @param direction input received from any keyboard-like input system
+   * @param delta_time produced by the engine.
+   */
+  void ProcessKeyboard(CameraMovement direction, double delta_time);
+
+  /**
+   * Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+   * @param x_offset the mouses x offset.
+   * @param y_offset the mouse y offset
+   * @param constrain_pitch ensures the camera cant be flipped.
+   */
+  void ProcessMouseMovement(double x_offset, double y_offset,
+                            GLboolean constrain_pitch = true);
+
+  /**
+   * Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+   * @param y_offset the offset of the mouse.
+   */
+  void ProcessMouseScroll(double y_offset);
+
+  /**
+   * Gets the location of the camera on the x and y axis.
+   * @return the location of the camera
+   */
+  glm::dvec2 GetLocation() const;
+
+  /**
+   * The right vector of the camera
+   * @return right vector
+   */
+  glm::vec3 GetRightVector();
+
+  void ProcessKeyboardInput(bool forward, bool backward, bool left,
+                            bool right, double delta_time);
+  /**
+   * Calculates the front vector from the Camera's (updated) Euler Angles
+   */
+  void UpdateCameraVectors();
+};
+
+}  // namespace engine

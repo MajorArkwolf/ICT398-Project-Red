5b4c8f0765f2f1dbf37fa1a993244a69b3fe5667
William Vlahos <wxavierv@gmail.com>
2020-08-05 15:29:32 +0800

    Preliminary style guide compliance changes for ecs.h and ecs.cc.

---
+++ src/ECS/ecs.cc
@@ -0,0 +1,20 @@
+#include "ecs.h"
+
+#include "ECS/Entity.hpp"
+#include "ECS/System/System.hpp"
+
+void ECS::FixedUpdate(double t, double dt) {
+}
+
+void ECS::Update(double t, double dt) {
+  System::UpdateAnimation(registry, t, dt);
+}
+
+void ECS::Draw(const glm::mat4& projection, const glm::mat4& view, const glm::dvec2& cameraPos) {
+  System::Draw(registry, projection, view);
+}
+
+Entity& ECS::CreateEntity() {
+  this->entity_register.emplace_back(std::make_shared<Entity>(registry.create(), this));
+  return *entity_register.at(entity_register.size() - 1).get();
+}

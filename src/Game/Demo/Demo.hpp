24cf9cd7dd082122f2b2e9176c6e57b065bd5aa8
William Vlahos <wxavierv@gmail.com>
2020-08-06 11:27:56 +0800

    Style guide compliance changes for Demo.hpp and Demo.cpp. No new errors or change in behaviour was generated as a result of this refactoring.

--- src/Game/Demo/Demo.hpp
+++ src/Game/Demo/Demo.hpp
@@ -1,20 +1,20 @@
 #pragma once
+
 #include "ECS/ECS.hpp"
 #include "Game/BaseState.hpp"
 
 class Demo : public BaseState {
-public:
-    Demo();
-    ~Demo() override = default;
-    void Init() override;
-    void UnInit() override;
-    void Display(const glm::mat4& projection, const glm::mat4& view) override;
-    void GUIStart() override;
-    void GUIEnd() override;
-    void Update(double t, double dt) override;
-    void FixedUpdate(double t, double dt) override;
+ public:
+  Demo();
+  ~Demo() override = default;
+  void Init() override;
+  void UnInit() override;
+  void Display(const glm::mat4& projection, const glm::mat4& view) override;
+  void GUIStart() override;
+  void GUIEnd() override;
+  void Update(double t, double dt) override;
+  void FixedUpdate(double t, double dt) override;
 
-private:
-    ECS ecs = {};
+ private:
+  ECS ecs = {};
 };
-

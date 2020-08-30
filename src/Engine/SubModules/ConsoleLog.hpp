5d6e10b352116e6085ec878641884cb986c70786
Kiera Gibson <kierajgibson0@gmail.com>
2020-08-30 22:31:23 +0800

    add comments

--- src/Engine/SubModules/ConsoleLog.hpp
+++ src/Engine/SubModules/ConsoleLog.hpp
@@ -23,8 +23,23 @@ class ConsoleLog {
     ConsoleLog() = default;
     ~ConsoleLog() = default;
 
+    /**
+    * @brief Adds a new log to the log console and file output
+    * @param type The type of log that it is
+    * @param text The text displayed in the log output
+    * @param line_number The Line number of the code that the log is added, use the macro __LINE__
+    * @param file The file that the log was added from , use the macro __FILE__
+    */
     void AddLog(LogType type, const std::string& text, int line_number = -1, std::string file = "");
+
+    /**
+    * @brief returns the log file so things that need to search through it can
+    */
     const std::vector<LogLine>& GetLogFile();
+    /**
+    * Starts the log generation by creating the log file
+    * @param the base_path to create the logs from  e.g.  base_path/logs/log.txt
+    */
     void StartLog(std::string base_path);
 
     ConsoleLog(const ConsoleLog&) = delete;

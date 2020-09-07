a2d2602edc508ec8dc345f2a975ee303fe15158a
Peter <petercrabbe92@gmail.com>
2020-09-07 21:49:03 +0800

    Fix texture loader

--- src/Engine/Renderer/OpenGL.cpp
+++ src/Engine/Renderer/OpenGL.cpp
@@ -145,7 +145,8 @@ void view::OpenGL::ResizeWindow() {
 unsigned int view::OpenGL::TextureFromFile(const std::string &path, const std::filesystem::path &directory,
                                            [[maybe_unused]] bool gamma) {
     auto new_dir = directory;
-    std::filesystem::path filename = new_dir.remove_filename() / path;
+    std::filesystem::path ext = path;
+    std::filesystem::path filename = new_dir.remove_filename() / ext.filename();
 
     unsigned int texture_id = 0;
     glGenTextures(1, &texture_id);

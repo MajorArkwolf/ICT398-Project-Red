85e281fcb1ec74a10e44d9125b4c82783772e1a5
Peter <petercrabbe92@gmail.com>
2020-09-10 23:18:24 +0800

    add doxygen for the physics engine.

--- src/Engine/Physics/CollisionDetection.hpp
+++ src/Engine/Physics/CollisionDetection.hpp
@@ -14,15 +14,29 @@ namespace physics {
 
     class RedEngineEventListener : public reactphysics3d::EventListener {
     public:
+        //Default Constructor
         RedEngineEventListener() = default;
 
+        /**
+         * Main constructor used.
+         * @param c_e_c A map to look up collision bodies into entities.
+         */
         RedEngineEventListener(std::unordered_map<reactphysics3d::CollisionBody *, entt::entity> *c_e_c);
 
+        /**
+         * Overloaded function that allows us to handle the collision detection
+         * @param callbackData call back function pointer.
+         */
         void onContact(const reactphysics3d::CollisionCallback::CallbackData &callbackData) override;
 
+        /**
+         * Gets the collision detection que.
+         * @return
+         */
         std::queue<PhysicsCollisionData> &GetPhysicsQueue();
-
+        /// Used to look up entities from collision bodies.
         std::unordered_map<reactphysics3d::CollisionBody *, entt::entity> *collision_entity_coupling_ = nullptr;
+        /// The que that will be used for handling entities.
         std::queue<PhysicsCollisionData> physics_que_ = {};
     };
 
@@ -33,14 +47,36 @@ namespace physics {
          */
         CollisionDetection();
 
+        /**
+         * Default Destructor.
+         */
         ~CollisionDetection();
 
+        /**
+         * Toggles the collision renderer.
+         */
         void ToggleRenderer();
 
+        /**
+         * Adds a collision body to the detection.
+         * @param entity_id the id of the entity
+         * @param pos the position of said entity.
+         * @param rot the rotation of said entity.
+         */
         void AddCollisionBody(const entt::entity &entity_id, const glm::vec3 &pos, const glm::quat &rot);
 
+        /**
+         * Updates the collision body location
+         * @param entity_id the id of the entity to update.
+         * @param pos the current position of the entity.
+         * @param rot the rotation of the current entity.
+         */
         void UpdateCollisionBody(const entt::entity &entity_id, const glm::vec3 &pos, const glm::quat &rot);
 
+        /**
+         * Delete colission body both from the world and from the maps.
+         * @param entity_id the entity id to drop.
+         */
         void DeleteCollisionBody(const entt::entity &entity_id);
 
         void Draw(const glm::mat4 &projection, const glm::mat4 &view);
@@ -53,31 +89,46 @@ namespace physics {
 
         physics::PhysicsShape CreateSphereShape(double radius);
 
+
         int AddCollider(const entt::entity &entity_id, physics::PhysicsShape &shape, glm::vec3 relative_position,
                         glm::quat rotation);
 
+
+        /// This que is used for handling collisions both detection and resolution.
         std::queue<PhysicsCollisionData> &GetCollisions();
 
+        /// Gets the render status of the renderer.
         bool GetRendererStatus() const;
 
     private:
+
+        /// Value to enable or disable renderering.
         bool renderer_ = false;
 
         //Test Renderer Values
         unsigned int l_vbo_ = 0, l_vao_ = 0;
         unsigned int t_vbo_ = 0, t_vao_ = 0;
         unsigned int line_num_ = 0, triag_num_ = 0;
+
+        /// The shared pointer to draw react objects,
         std::unique_ptr<Shader> shader_ = nullptr;
 
-
+        /// The react PhysicsCommon
         reactphysics3d::PhysicsCommon *physics_common_ = nullptr;
+
+        /// The react PhysicsWorld
         reactphysics3d::PhysicsWorld *world_ = nullptr;
+
+        /// The event lister in react to catch collission points.
         RedEngineEventListener event_listener_;
+
+        /// Map lookup from an entity to a collission body.
         std::unordered_map<entt::entity, reactphysics3d::CollisionBody *> entity_collision_coupling_ = {};
+        /// Map lookup from a collission body to an entity.
         std::unordered_map<reactphysics3d::CollisionBody *, entt::entity> collision_entity_coupling_ = {};
 
         /**
-         * Creates a relationship bidirectional relationship between entity and a collision body for fast lookup times.
+         * Creates a bidirectional relationship between entity and a collision body for fast lookup times.
          * @param entity the entity id
          * @param coll_body pointer to the react physics body
          */
--- src/Engine/Physics/PhysicsEngine.hpp
+++ src/Engine/Physics/PhysicsEngine.hpp
@@ -8,28 +8,78 @@ namespace physics {
 
     class PhysicsEngine {
     public:
+        /**
+         * Default constructor.
+         */
         PhysicsEngine() = default;
 
+        /**
+         * Default destructor.
+         */
         ~PhysicsEngine() = default;
 
+        /**
+         * Deleted copy constructor.
+         * @param p
+         */
         PhysicsEngine(PhysicsEngine &p) = delete;
 
+        /**
+         * Deleted move constructor
+         * @param p
+         */
         PhysicsEngine(PhysicsEngine &&p) = delete;
 
+        /**
+         * Deleted copy assignment.
+         * @param p
+         * @return
+         */
         PhysicsEngine &operator=(PhysicsEngine p) = delete;
 
+        /**
+         * Deleted move assignment.
+         * @param p
+         * @return
+         */
         PhysicsEngine &operator=(PhysicsEngine &&p) = delete;
 
+        /**
+         * Fixed Update, commonly used to update physics based components.
+         * @param t time since engine started.
+         * @param dt time since last frame aka delta time.
+         */
         void FixedUpdate(double t, double dt);
 
+        /**
+         * This is used for non physics related tasks such as setting up the display renderer for the draw call.
+         * @param t time since engine started.
+         * @param dt time since last frame aka delta time.
+         */
         void Update(double t, double dt);
 
+        /**
+         * Draws the physics world over the real world.
+         * @param projection projection matrix of the camera.
+         * @param view the view matrix of the camera.
+         */
         void Draw(const glm::mat4 &projection, const glm::mat4 &view);
 
+        /**
+         * Toggles the debug renderer on or off.
+         */
         void ToggleRenderer();
 
+        /**
+         * Returns if the renderer is on.
+         * @return true if renderer is on.
+         */
         bool GetRendererStatus();
 
+        /**
+         * Set the ECS for the Physics engine to use, this is crucial and must be done.
+         * @param ecs ECS system.
+         */
         void SetECS(ECS *ecs);
 
         PhysicsShape CreateBoxShape(glm::vec3 extents);

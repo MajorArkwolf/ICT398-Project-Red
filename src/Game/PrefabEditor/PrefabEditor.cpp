88b5e6a937d8580ee680249eb0ef8e0e574c7466
Kiera Gibson <kierajgibson0@gmail.com>
2020-09-16 19:54:38 +0800

    add rotate camera

--- src/Game/PrefabEditor/PrefabEditor.cpp
+++ src/Game/PrefabEditor/PrefabEditor.cpp
@@ -174,6 +174,9 @@ void PrefabEditor::HandleInputData(input::InputEvent inputData, double deltaTime
                     case input::InputType::kMouseScrolled: {
                         double amountScrolledY = static_cast<double>(vec.y);
                         distanceFromEntity += amountScrolledY;
+                        if (distanceFromEntity < 0) {
+                            distanceFromEntity = 0;
+                        }
                     }
                         break;
                     default:
@@ -189,37 +192,44 @@ void PrefabEditor::HandleInputData(input::InputEvent inputData, double deltaTime
 void PrefabEditor::ArcBallCamera(double dx, double dy) {
     if (camera_enabled_) {
         auto viewport = redengine::Engine::get().renderer_.GetViewPort();
-// Get the homogenous position of the camera and pivot point
-        glm::vec4 position = {camera.position_, 1.0f};
-        glm::vec4 pivot = {(camera.position_ + camera.front_), 1.0f};
-        //glm::vec4 position(app->m_camera.GetEye().x, app->m_camera.GetEye().y, app->m_camera.GetEye().z, 1);
-        //glm::vec4 pivot(app->m_camera.GetLookAt().x, app->m_camera.GetLookAt().y, app->m_camera.GetLookAt().z, 1);
 
-// step 1 : Calculate the amount of rotation given the mouse movement.
-        float deltaAngleX = (2 * M_PI / viewport.x); // a movement from left to right = 2*PI = 360 deg
-        float deltaAngleY = (M_PI / viewport.y);  // a movement from top to bottom = PI = 180 deg
-        float xAngle = static_cast<float>(dx) * deltaAngleX;
-        float yAngle = static_cast<float>(dx) * deltaAngleY;
-
-// Extra step to handle the problem when the camera direction is the same as the up vector
-        float cosAngle = dot(camera.front_, camera.up_);
-        //float cosAngle = dot(app->m_camera.GetViewDir(), app->m_upVector);
-        //TODO: Reimplement this, What is yDeltaAngle? http://asliceofrendering.com/camera/2019/11/30/ArcballCamera/
-        if (cosAngle * sgn(yDeltaAngle) > 0.99f)
-            yAngle = 0;
-
-// step 2: Rotate the camera around the pivot point on the first axis.
-        glm::mat4x4 rotationMatrixX(1.0f);
-        rotationMatrixX = glm::rotate(rotationMatrixX, xAngle, camera.up_); // COULD BE WRONG
-        position = (rotationMatrixX * (position - pivot)) + pivot;
-
-// step 3: Rotate the camera around the pivot point on the second axis.
-        glm::mat4x4 rotationMatrixY(1.0f);
-        rotationMatrixY = glm::rotate(rotationMatrixY, yAngle, camera.GetRightVector()); // COULD BE WRONG
-        glm::vec3 finalPosition = (rotationMatrixY * (position - pivot)) + pivot;
-
-        // Update the camera view (we keep the same lookat and the same up vector)
-        camera.SetCameraView(finalPosition, camera.position_, camera.up_);
-        //app->m_camera.SetCameraView(finalPosition, app->m_camera.GetLookAt(), app->m_upVector);
+        auto rotation = currentRotation * ((std::atan(1) * 4) / 180);
+        camera.position_.x = std::cos(rotation) * distanceFromEntity;
+        camera.position_.z = std::sin(rotation) * distanceFromEntity;
+        camera.position_.y += dy;
+        currentRotation = fmod(currentRotation + dx, 360);
+        camera.front_ = -camera.position_;
+        //// Get the homogenous position of the camera and pivot point
+//        glm::vec4 position = {camera.position_, 1.0f};
+//        glm::vec4 pivot = {(camera.position_ + camera.front_), 1.0f};
+//        //glm::vec4 position(app->m_camera.GetEye().x, app->m_camera.GetEye().y, app->m_camera.GetEye().z, 1);
+//        //glm::vec4 pivot(app->m_camera.GetLookAt().x, app->m_camera.GetLookAt().y, app->m_camera.GetLookAt().z, 1);
+//
+//// step 1 : Calculate the amount of rotation given the mouse movement.
+//        float deltaAngleX = (2 * M_PI / viewport.x); // a movement from left to right = 2*PI = 360 deg
+//        float deltaAngleY = (M_PI / viewport.y);  // a movement from top to bottom = PI = 180 deg
+//        float xAngle = static_cast<float>(dx) * deltaAngleX;
+//        float yAngle = static_cast<float>(dx) * deltaAngleY;
+//
+//// Extra step to handle the problem when the camera direction is the same as the up vector
+//        float cosAngle = dot(camera.front_, camera.up_);
+//        //float cosAngle = dot(app->m_camera.GetViewDir(), app->m_upVector);
+//        //TODO: Reimplement this, What is yDeltaAngle? http://asliceofrendering.com/camera/2019/11/30/ArcballCamera/
+//        if (cosAngle * sgn(yDeltaAngle) > 0.99f)
+//            yAngle = 0;
+//
+//// step 2: Rotate the camera around the pivot point on the first axis.
+//        glm::mat4x4 rotationMatrixX(1.0f);
+//        rotationMatrixX = glm::rotate(rotationMatrixX, xAngle, camera.up_); // COULD BE WRONG
+//        position = (rotationMatrixX * (position - pivot)) + pivot;
+//
+//// step 3: Rotate the camera around the pivot point on the second axis.
+//        glm::mat4x4 rotationMatrixY(1.0f);
+//        rotationMatrixY = glm::rotate(rotationMatrixY, yAngle, camera.GetRightVector()); // COULD BE WRONG
+//        glm::vec3 finalPosition = (rotationMatrixY * (position - pivot)) + pivot;
+//
+//        // Update the camera view (we keep the same lookat and the same up vector)
+//        camera.SetCameraView(finalPosition, camera.position_, camera.up_);
+//        //app->m_camera.SetCameraView(finalPosition, app->m_camera.GetLookAt(), app->m_upVector);
     }
 }

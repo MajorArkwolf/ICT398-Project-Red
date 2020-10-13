d48323773e87aef219ff2ea97f46838a51f13517
Peter Crabbe <petercrabbe92@gmail.com>
2020-10-13 21:38:19 +0800

    Add error check on animation node

--- src/ECS/System/AISystem.cpp
+++ src/ECS/System/AISystem.cpp
@@ -1,11 +1,20 @@
 #include "AISystem.hpp"
 #include "ECS/Component/Basic.hpp"
 #include "ECS/Component/Node.hpp"
+#include "ECS/Component/Pathing/Node.hpp"
 #define EPSILON 0.1f
+
+static inline void AlterNodeGrid(Pathing::Node * node, bool occupied) {
+    if (node != nullptr) {
+        node->occupied = occupied;
+    }
+}
 
 void System::AISystem::Moving(entt::registry &ecs, double t, double dt) {
     auto entities = ecs.view<component::Moving, component::Transform>();
+    // Iterate over all the components that have a moving and a transform component
     for (auto &e : entities) {
+        // We store the moving component then check if the there is a que.
         auto &move = entities.get<component::Moving>(e);
         if (!move.move_list.empty()) {
             if (ecs.has<component::Transform>(e)) {
@@ -13,8 +22,11 @@ void System::AISystem::Moving(entt::registry &ecs, double t, double dt) {
                 auto entity_id = move.move_list.front();
                 auto moving_to = ecs.get<component::Transform>(entity_id).pos;
                 auto dist = moving_to - tran.pos;
+                // We check to see if the object is within a certain epsilion before determining it is at a given node.
                 if (!(abs(dist.x) < EPSILON && abs(dist.z) < EPSILON)) {
-                    ecs.get<component::Node>(entity_id).n_o = node_occupancy::occupied;
+                    auto &node_comp = ecs.get<component::Node>(entity_id);
+                    node_comp.n_o = node_occupancy::occupied;
+                    AlterNodeGrid(node_comp.grid_node, true);
                     glm::vec3 dir = glm::normalize(moving_to - tran.pos);
                     dir = dir * (move.speed * static_cast<float>(dt));
                     tran.pos += dir;
@@ -23,14 +35,15 @@ void System::AISystem::Moving(entt::registry &ecs, double t, double dt) {
                     tran.rot = quat;
                     if (ecs.has<component::Animation>(e)) {
                         auto &anim = ecs.get<component::Animation>(e);
-                        auto name = anim.animator_.loaded_animation_->GetName();
                         anim.animator_.LoadAnimation("WALK", false);
                     }
                 } else {
+                    auto &node_comp = ecs.get<component::Node>(entity_id);
                     if (ecs.has<component::Node>(entity_id)) {
                         ecs.get<component::Node>(entity_id).n_o = node_occupancy::leaving;
                     }
                     if (ecs.has<component::Node>(move.last_node)) {
+                        AlterNodeGrid(node_comp.grid_node, false);
                         ecs.get<component::Node>(move.last_node).n_o = node_occupancy::vacant;
                     }
                     move.last_node = move.move_list.front();
@@ -40,7 +53,11 @@ void System::AISystem::Moving(entt::registry &ecs, double t, double dt) {
         } else {
             if (ecs.has<component::Animation>(e)) {
                 auto &anim = ecs.get<component::Animation>(e);
-                anim.animator_.LoadAnimation("IDLE", false);
+                if (anim.animator_.loaded_animation_ != nullptr) {
+                    if (anim.animator_.loaded_animation_->GetName() == "WALK") {
+                        anim.animator_.LoadAnimation("IDLE", false);
+                    }
+                }
             }
         }
     }

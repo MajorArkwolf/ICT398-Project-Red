91f66491ef26981c1504600aab285cb8b4d31c11
William Vlahos <wxavierv@gmail.com>
2020-10-16 16:25:07 +0800

    - Added header file for the core ECS systems nessecary to operate the NPCs. - Added stubbed implementations for the new NPC ECS systems.

---
+++ src/ECS/System/NPC/Core.cpp
@@ -0,0 +1,37 @@
+#include "Core.hpp"
+
+namespace System {
+
+void NPCInit(entt::entity& entity) {
+
+}
+
+void NPCUnInit(entt::entity& entity) {
+
+}
+
+void NPCImport(entt::entity& entity, std::string path) {
+
+}
+
+void NPCExport(entt::entity& entity, std::string path) {
+
+}
+
+void NPCObserve(entt::registry& registry, entt::entity& entity) {
+
+}
+
+void NPCPrepare(entt::registry& registry, entt::entity& entity) {
+
+}
+
+void NPCRespond(entt::registry& registry, entt::entity& entity) {
+
+}
+
+void NPCsFixedUpdate(entt::registry& registry, double t, double dt) {
+
+}
+
+} // namespace System
---
+++ src/ECS/System/NPC/Core.hpp
@@ -0,0 +1,64 @@
+#include <entt/entt.hpp>
+
+namespace System {
+
+    /**
+     * @brief Sets an Entity's NPC components to their default values.
+     * Also generates any NPC components the Entity is missing.
+     * @param [in,out] entity The target Entity.
+     */
+void NPCInit(entt::entity& entity);
+
+    /**
+     * @brief Deletes all NPC components from an Entity.
+     * Only deletes the NPC components, and does not delete the Entity itself.
+     * @param [in,out] entity The target Entity.
+     */
+void NPCUnInit(entt::entity& entity);
+
+    /**
+     * @brief Sets an Entity's NPC components to values from an external file.
+     * Also generates any NPC components the Entity is missing.
+     * @param [in,out] entity The target Entity.
+     * @param [in] path The filesystem path to the target external file.
+     */
+void NPCImport(entt::entity& entity, std::string path);
+
+    /**
+     * @brief Exports an Entity's NPC component values to an external file.
+     * @param [in,out] entity The target Entity.
+     * @param [in] path The filesystem path to the target external file.
+     */
+void NPCExport(entt::entity& entity, std::string path);
+
+    /**
+     * @brief Updates an NPC's Beliefs and Desires from its perception of its environment.
+     * @param [in,out] registry The target Registry.
+     * @param [in,out] entity The target Entity.
+     */
+void NPCObserve(entt::registry& registry, entt::entity& entity);
+
+    /**
+     * @brief Updates an NPC's Intentions and Sub-Desires from its Beliefs and Desires.
+     * @param [in,out] registry The target Registry.
+     * @param [in,out] entity The target Entity.
+     */
+void NPCPrepare(entt::registry& registry, entt::entity& entity);
+
+    /**
+     * @brief Tests an NPC's Intentions and responds to those triggered with Actions.
+     * @param [in,out] registry The target Registry.
+     * @param [in,out] entity The target Entity.
+     */
+void NPCRespond(entt::registry& registry, entt::entity& entity);
+
+    /**
+     * @brief Updates all NPC Entity components and behaviours.
+     * Automatically manages calls to AI inference, observation, and response systems.
+     * @param [in,out] registry The target Registry.
+     * @param [in] t The current time.
+     * @param [in] dt The delta time (time step) from previous update.
+     */
+void NPCsFixedUpdate(entt::registry& registry, double t, double dt);
+
+} // namespace System

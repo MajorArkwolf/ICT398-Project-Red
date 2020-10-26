cc941da1118c16cc6e4b61c229522249e74b96ae
Peter <petercrabbe92@gmail.com>
2020-10-27 00:38:35 +0800

    Use physics system for node tracking

--- src/ECS/Component/Node.cpp
+++ src/ECS/Component/Node.cpp
@@ -3,7 +3,7 @@
 #include "ECS/Component/Basic.hpp"
 
 void component::Node::AlterNodeGrid(node_occupancy current) {
-    n_o = current;
+    //n_o = current;
     if (current == node_occupancy::occupied || current == node_occupancy::leaving) {
         if (grid_node != nullptr) {
             grid_node->occupied = true;
@@ -17,12 +17,14 @@ void component::Node::AlterNodeGrid(node_occupancy current) {
 
 void component::Node::onTrigger(entt::registry &reg, entt::entity ent, PhysicsTriggerData::Event event) {
     if (reg.has<component::InteractableObject>(ent)) {
-        if (reg.get<component::InteractableObject>(ent).type != InteractableObject::Type::npc) {
+        if (/**reg.get<component::InteractableObject>(ent).type != InteractableObject::Type::npc**/ true) {
             if (event == PhysicsTriggerData::Event::start || event == PhysicsTriggerData::Event::stay) {
                 this->AlterNodeGrid(node_occupancy::occupied);
+                n_o = node_occupancy::occupied;
                 current_obj = ent;
             } else if (event == PhysicsTriggerData::Event::exit) {
                 this->AlterNodeGrid(node_occupancy::vacant);
+                n_o = node_occupancy::vacant;
             }
         }
     }
